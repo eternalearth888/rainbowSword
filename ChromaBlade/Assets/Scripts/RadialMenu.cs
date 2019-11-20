e0bfdca1228b16dce9bcaeb393c0f07934062a75
Maria Deslis <mdeslis@gmail.com>
2019-11-19 22:35:04 -0700

    Basic radial menu click, more to come

---
+++ ChromaBlade/Assets/Scripts/RadialMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenu : MonoBehaviour
+{
+    public RadialButton buttonPrefab;
+    public RadialButton selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RadialButton newButton = Instantiate(buttonPrefab) as RadialButton;
+        newButton.transform.SetParent(transform, false);
+        newButton.transform.localPosition = new Vector3(0f, 100f, 0f);
+    }
+}
---
+++ ChromaBlade/Assets/Scripts/RadialMenuSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenuSpawner : MonoBehaviour
+{
+
+    public static RadialMenuSpawner ins;
+    public RadialMenu menuPrefab;
+
+
+    void Awake()
+    {
+        ins = this;   
+    }
+
+    // Interactable calls this to create the menu
+    public void SpawnMenu()
+    {
+        RadialMenu newMenu = Instantiate(menuPrefab) as RadialMenu;
+        // plop it in the middle of the screen
+        newMenu.transform.SetParent(transform, false);
+        // plop it where the mouse has clicked
+        newMenu.transform.position = Input.mousePosition;
+    }
+
+}
