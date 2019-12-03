da98dcffd938f643f06e70a22b2d444e5040b1d2
Willie <willie.kirby14@gmail.com>
2019-12-03 16:40:32 -0700

    Starting Work on Enemy Behavior

---
+++ ChromaBlade/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject playerObj;
+    private Vector3 playerPos;
+    public float MoveSpeed;
+    public float MaxDist;
+    public float MinDist;
+    private Animator anim;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = playerObj.transform.position;
+        transform.LookAt(playerPos);
+
+        if(Vector3.Distance(transform.position, playerPos) >= MinDist){
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            //activate walking animation here
+            
+        }
+
+        if(Vector3.Distance(transform.position, playerPos) <= MaxDist){
+            //call attacking code here
+        }
+    }
+}
