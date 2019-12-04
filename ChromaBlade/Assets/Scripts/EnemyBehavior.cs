64500f7827b809145ee4b96e90467d388143afd9
Willie <willie.kirby14@gmail.com>
2019-12-04 14:53:19 -0700

    Fixing Beetle Animations

--- ChromaBlade/Assets/Scripts/EnemyBehavior.cs
+++ ChromaBlade/Assets/Scripts/EnemyBehavior.cs
@@ -10,6 +10,8 @@ public class EnemyBehavior : MonoBehaviour
     public float MaxDist;
     public float MinDist;
     private Animator anim;
+    private bool isWalk;
+    private float walkTimer;
 
     
     
@@ -25,14 +27,28 @@ public class EnemyBehavior : MonoBehaviour
         playerPos = playerObj.transform.position;
         transform.LookAt(playerPos);
 
+        AnimatorClipInfo[] ci;
+        ci = anim.GetCurrentAnimatorClipInfo(0);
+
         if(Vector3.Distance(transform.position, playerPos) >= MinDist){
             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
             //activate walking animation here
-            
+            if (ci[0].clip.name != "Run Forward In Place"){
+                // Avoid any reload.
+                Debug.Log(ci[0].clip.name);
+                walkAnim();
+            }
         }
 
         if(Vector3.Distance(transform.position, playerPos) <= MaxDist){
             //call attacking code here
+            //anim.CrossFade("Idle", 0);
         }
+    }
+
+    void walkAnim(){
+        //anim.StopPlayback();
+        anim.CrossFade("Run Forward In Place", 0);
+        
     }
 }
