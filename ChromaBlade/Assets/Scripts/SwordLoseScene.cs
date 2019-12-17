db049d108069d4d3a4e333abdcfb4c37ae574f29
Maria Deslis <mdeslis@gmail.com>
2019-12-16 20:14:17 -0700

    new color crystals

---
+++ ChromaBlade/Assets/Scripts/SwordLoseScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordLoseScene : MonoBehaviour
+{
+
+    public GameObject[] prefabs;
+    private GameObject endSword;
+    private int lastPick = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPick = SwordSwapper.selectedSword;
+        endSword = Instantiate(prefabs[lastPick], transform.position, Quaternion.identity) as GameObject;
+
+    }
+
+}
